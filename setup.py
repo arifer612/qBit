5d483a0853b2cd580fc936fff32404c241e984bc
arifer612 <arifer1995@gmail.com>
2020-11-04 00:49:08 +0800

    Added MIT License

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+with open('./LICENSE', 'r') as f:
+    LICENSE = f.read()
+
+with open('./requirements.txt', 'r') as g:
+    requires = g.read().splitlines()
+
 setuptools.setup(
     name="qBit",
     version="unreleased",
     author="arifer612",
     author_email="arifer1995@gmail.com",
     description="A human-readable qBittorrent torrent manager",
+    license=LICENSE,
+    install_requires=requires,
     url="https://github.com/arifer612/qBit",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -17,4 +25,8 @@ setuptools.setup(
         "Natural Language :: English",
     ],
     python_requires='>=3.6',
+    keywords=[
+        'qBittorrent',
+        'torrent'
+    ]
 )
